89fa111f9775467dda416aa8bd6f83236ae306f1
m01otov <67689912+m01otov@users.noreply.github.com>
2021-11-06 11:56:28 +0500

    add icon image

--- AppGallery/Sources/Scenes/Gallery/Model/Model.swift
+++ AppGallery/Sources/Scenes/Gallery/Model/Model.swift
@@ -5,4 +5,26 @@
 //  Created by Ilya Melnikov on 06.11.2021.
 //
 
-import Foundation
+import UIKit
+
+struct Model {
+
+    var models = [Section]()
+
+    mutating func createCollection() {
+
+        // MARK: - My albums section
+
+        models.append(Section(header: Header.headerAndButton(model:
+                       HeaderAndButton(title: SectionTitle.myAlbums, titleButton: SectionTitle.buttonTitle) ), cell: [
+       .imageCell(model: CardTypeCell(header: MyAlbumsCellTitle.recent, icon: MyAlbumsCellIconImage.recent, countTitle: MyAlbumsCellSubTitle.recent)),
+        .imageCell(model: CardTypeCell(header: MyAlbumsCellTitle.favorites, icon: MyAlbumsCellIconImage.favorites, countTitle: MyAlbumsCellSubTitle.favorites)),
+       .imageCell(model: CardTypeCell(header: MyAlbumsCellTitle.instagram, icon: MyAlbumsCellIconImage.instagram, countTitle: MyAlbumsCellSubTitle.instagram)),
+        .imageCell(model: CardTypeCell(header: MyAlbumsCellTitle.whatsApp, icon: MyAlbumsCellIconImage.whatsApp, countTitle: MyAlbumsCellSubTitle.whatsApp)),
+       .imageCell(model: CardTypeCell(header: MyAlbumsCellTitle.whatsApp, icon: MyAlbumsCellIconImage.whatsApp, countTitle: MyAlbumsCellSubTitle.whatsApp)),
+
+
+
+
+                       ]))
+}
