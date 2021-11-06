82603c6c817371fb786cde65c3c31917024c6851
m01otov <67689912+m01otov@users.noreply.github.com>
2021-11-06 11:49:10 +0500

    Add to the name's  section

--- AppGallery/Sources/Scenes/Gallery/Model/Names.swift
+++ AppGallery/Sources/Scenes/Gallery/Model/Names.swift
@@ -6,3 +6,101 @@
 //
 
 import Foundation
+
+enum TabBarTitle {
+     static let mediaLibrary = "Медиатека"
+     static let forYou = "Для Вас"
+     static let gallery = "Альбомы"
+     static let search = "Поиск"
+ }
+
+enum SectionTitle {
+     static let myAlbums = "Мои альбомы"
+     static let sharedAlbums = "Общие альбомы"
+     static let peopleAndPlaces = "Люди и места"
+     static let typesOfMedia = "Типы медиафайлов"
+     static let other = "Другое"
+     static let buttonTitle = "Все"
+ }
+
+enum MyAlbumsCellTitle {
+     static let recent = "Недавние"
+     static let favorites = "Избранное"
+     static let instagram = "Instagram"
+     static let whatsApp = "WhatsApp"
+     static let pinterest = "Pinterest"
+     static let telegram = "Telegram"
+     static let twitter = "Twitter"
+     static let work = "Работа"
+}
+
+enum SharedAlbumsCellTitle {
+    static let family = "Семья"
+    static let girlFriend = "Настя"
+    static let general = "Общее"
+    static let home = "Дом"
+}
+
+enum PeopleAndPlacesCellTitle {
+     static let people = "Люди"
+     static let places = "Места"
+ }
+
+enum TypesOfMediaCellTitle {
+     static let video = "Видео"
+     static let selfie = "Селфи"
+     static let photoLivePhotos = "Фото Live Photos"
+     static let portraits = "Портреты"
+     static let panoramas = "Панорамы"
+     static let timelapse = "Таймлапс"
+     static let slowly = "Замедленно"
+     static let screenshots = "Снимки экрана"
+     static let animated = "Анимированные"
+ }
+
+enum OtherCellTitle {
+     static let imported = "Импортированные"
+     static let hidden = "Скрытые"
+     static let recentlyDeleted = "Недавно удаленные"
+ }
+
+enum MyAlbumsCellSubTitle {
+    static let recent = "4088"
+    static let favorites = "60"
+    static let instagram = "30"
+    static let whatsApp = "33"
+    static let pinterest = "40"
+    static let telegram = "125"
+    static let twitter = "15"
+    static let work = "77"
+}
+
+enum SharedAlbumsCellSubTitle {
+    static let family = "From You"
+    static let girlFriend = "From Настя"
+    static let general = "From You"
+    static let home = "From You"
+}
+
+enum PeopleAndPlacesCellSubTitle {
+     static let people = "30"
+     static let places = "1451"
+ }
+
+ enum TypesOfMediaCellSubTitle {
+     static let video = "150"
+     static let selfie = "233"
+     static let photoLivePhotos = "160"
+     static let portraits = "51826"
+     static let panoramas = "9"
+     static let timelapse = "3"
+     static let slowly = "23"
+     static let screenshots = "325"
+     static let animated = "2"
+ }
+
+enum OtherCellSubTitle {
+     static let imported = "279"
+     static let hidden = "18"
+     static let recentlyDeleted = "8"
+ }
