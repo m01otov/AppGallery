6e914fb5abcc5bcabce0ad5fb274642ef9bc0f92
m01otov <67689912+m01otov@users.noreply.github.com>
2021-11-06 10:49:46 +0500

    Initial Commit

---
+++ AppGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppGallery
+//
+//  Created by Ilya Melnikov on 06.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
