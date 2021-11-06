be798187a30321dba13a9e6512254c954f628ffa
m01otov <67689912+m01otov@users.noreply.github.com>
2021-11-06 11:57:14 +0500

    add model section

--- AppGallery/Sources/Scenes/Gallery/Model/ModelSections.swift
+++ AppGallery/Sources/Scenes/Gallery/Model/ModelSections.swift
@@ -5,4 +5,40 @@
 //  Created by Ilya Melnikov on 06.11.2021.
 //
 
-import Foundation
+import UIKit
+
+ struct CardTypeCell {
+     let header: String
+     let icon: UIImage?
+     let countTitle: String
+ }
+
+ struct RowTypeCell {
+     let header: String
+     let icon: UIImage?
+     let subTitle: String
+ }
+
+ enum SectionCellType {
+     case imageCell(model: CardTypeCell)
+     case rowCell(model: RowTypeCell)
+ }
+
+ struct Section {
+     let header: Header
+     let cell: [SectionCellType]
+ }
+
+ struct HeaderAndButton {
+     let title: String
+     let titleButton: String
+ }
+
+ struct HeaderTitle {
+     let title: String
+ }
+
+ enum Header {
+     case headerAndButton(model: HeaderAndButton)
+     case header(model: HeaderTitle)
+ }
