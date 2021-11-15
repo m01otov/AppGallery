f834231f990c489b276166c021eba3d75db1ed9d
m01otov <67689912+m01otov@users.noreply.github.com>
2021-11-15 12:49:23 +0500

    Create struct cell CollectionView

--- AppGallery/Sources/Scenes/Search/SearchViewController.swift
+++ AppGallery/Sources/Scenes/Search/SearchViewController.swift
@@ -9,4 +9,44 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    private let label: UILabel = {
+        let title = UILabel()
+
+        title.font = .systemFont(ofSize: 25, weight: .regular)
+        title.text = "Empty"
+        title.lineBreakMode = NSLineBreakMode.byWordWrapping
+        title.numberOfLines = 2
+        title.textAlignment = .center
+
+        return title
+    }()
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupHierarchy()
+        setupLayout()
+        setupView()
+    }
+
+    // MARK: - Settings
+
+    private func setupHierarchy() {
+        view.addSubview(label)
+    }
+
+    private func setupLayout() {
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        label.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.8).isActive = true
+        label.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+    }
+
+    private func setupView() {
+        view.backgroundColor = .systemPink
+    }
 }
+
